c02ebe4d542a82178386a7302e1f3ffcff55af92
schaecher <srschaecher@gmail.com>
2020-11-24 21:32:11 -0600

    Parameterized Slack and Database connections for processing multiple regions.

--- PAXcharter_Monthly_Execution.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-echo "Executing PAXcharter"
-cd /Users/schaecher/PycharmProjects/PAXminer/
-date >> ./PAXminer.log
-./PAXcharter.py
-echo "Executing BDminer"
-./BDminer.py
-echo "Executing PAXminer"
-./PAXminer.py
--- PAXminer_Daily_Execution.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-echo "Executing F3SlackUserLister"
-cd /Users/schaecher/PycharmProjects/PAXminer/
-date >> ./PAXminer.log
-./F3SlackUserLister.py
-echo "Executing BDminer"
-./BDminer.py
-echo "Executing PAXminer"
-./PAXminer.py
